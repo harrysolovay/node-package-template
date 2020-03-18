6d94e1e75fb000bae59fe4279db1c1af2625279a
Harry Solovay <harrysolovay@gmail.com>
2020-03-17 17:55:47 -0700

    chore: drop pika and specify separate tsc processes

---
+++ src/cli.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import {api} from ".";
+
+console.log(api());
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 export const api = (): string => {
   return "Hello World";
 };
-
-export const cli = (): void => {
-  console.log(api());
-};
