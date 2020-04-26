d0a3c7093e68e1211c4d358a24bd6a29b38ea416
Harry Solovay <harrysolovay@gmail.com>
2020-04-26 15:45:37 -0700

    chore: delete tsdocked class that was used solely for testing typedoc

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,3 @@
 export const api = (): string => {
   return "Hello World";
 };
-
-/**
- * Some description `Statistics` class
- */
-export class Statistics {
-  /**
-   * Returns the average of two numbers
-   *
-   * @param x - The first input number
-   * @param y - The second input number
-   * @returns The arithmetic mean of `x` and `y`
-   *
-   * @beta
-   */
-  public static getAverage(x: number, y: number): number {
-    return (x + y) / 2;
-  }
-}
