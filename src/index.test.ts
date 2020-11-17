824ea254ab6b228c0b80bd7fd354e1132aadbe76
Harry Solovay <harrysolovay@gmail.com>
2020-11-17 15:12:47 -0500

    chore: clean up testing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,10 @@
 import {api} from ".";
 
-describe("test block 1", () => {
-  it("test 1", () => {
+describe("Test Group 1", () => {
+  it("Test 1", () => {
     expect.assertions(1);
+    setTimeout(() => {}, 1000);
+    console.log("Some log from 'Test 1'");
     expect(api()).toStrictEqual("Hello World");
   });
 });
--- src/tests/api.ts
+++
@@ -1,8 +0,0 @@
-import {api} from "..";
-
-describe("test block 2", () => {
-  it("test 1", () => {
-    expect.assertions(1);
-    expect(api()).toStrictEqual("Hello World");
-  });
-});
